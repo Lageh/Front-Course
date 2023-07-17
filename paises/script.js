85c91c754736695c9fabc54e00b3bdff4640c3d8
Guilherme Guerra <guilherme0895@gmail.com>
2023-07-17 00:48:25 -0300

    Países

--- paises/script.js
+++ paises/script.js
@@ -5,7 +5,6 @@ function novoPais() {
     .then((response) => response.json())
     .then((data) => {
         
-        console.log(data)
         for (let i = 0; i < data.length; i++){
             
             let option = document.createElement("option")
@@ -23,8 +22,15 @@ function alerta(){
     let lingua = document.getElementById("lingua")
     let capital = document.getElementById("capital")
     let bandeira = document.getElementById("bandeira")
+    let fronteiras = document.getElementById("fronteiras")
+    let area = document.getElementById("area")
+    let moeda = document.getElementById("moeda")
+    let moeda_aux = []
     let aux = []
+    let fronteira_aux = []
     lingua.innerText = ""
+
+
     fetch("https://restcountries.com/v3.1/name/" + select+ "?fullText=true")
     .then((response) => response.json())
     .then((data) => {
@@ -37,6 +43,28 @@ function alerta(){
         lingua.innerHTML = aux.join(", ");
         capital.innerHTML = data[0].capital;
         bandeira.src = data[0].flags.png;
+        for (const key in data[0].borders) {
+            if(Object.hasOwnProperty.call(data[0].borders,key)){
+                if(!fronteira_aux.includes(data[0].borders[key])){
+                fronteira_aux.push(data[0].borders[key]);
+            }
+        }
+    }
+        if(fronteira_aux.length > 1 ){
+            fronteiras.innerHTML = fronteira_aux.join(", ");
+        }else{
+            fronteiras.innerHTML = fronteira_aux;
+        }
+        area.innerHTML = data[0].area + " km²";
+        
+
+        for (const key in data[0].currencies) {
+            moeda_aux.push(key);
+        }
+        moeda.innerHTML = moeda_aux.join(", ");
+        
+    
+        document.body.style.backgroundImage = "url(" + data[0].flags.png +")";
     })
 
 
