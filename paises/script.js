bf42c0021ebb6dc17c33c12205abdebebad98cfc
Guilherme Guerra <guilherme0895@gmail.com>
2023-07-16 23:45:28 -0300

    Países

--- paises/script.js
+++ paises/script.js
@@ -21,6 +21,8 @@ function novoPais() {
 function alerta(){
     let select = document.getElementById("comboPais").value
     let lingua = document.getElementById("lingua")
+    let capital = document.getElementById("capital")
+    let bandeira = document.getElementById("bandeira")
     let aux = []
     lingua.innerText = ""
     fetch("https://restcountries.com/v3.1/name/" + select+ "?fullText=true")
@@ -29,10 +31,14 @@ function alerta(){
         let idiomas = data[0].languages
         for (const key in idiomas) {
             if (Object.hasOwnProperty.call(idiomas, key)) {
-                aux.add(idiomas[key])
+                aux.push(idiomas[key])
             }
         }
-        console.log(aux)
+        lingua.innerHTML = aux.join(", ");
+        capital.innerHTML = data[0].capital;
+        bandeira.src = data[0].flags.png;
     })
+
+
 
 }
